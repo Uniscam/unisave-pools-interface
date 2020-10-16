37dcac49bfa92aa6b7ab49b6e7d388a91a88e0da
Ayaka Neko <neko@ayaka.moe>
2020-10-17 01:24:35 +0800

    :ambulance: Fixed staking token

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -22,7 +22,7 @@ export const Pools = [
             56: '0xB10fD0F525cBDeCef941a594dD7808427B3e87e2',
         },
         stakingTokenAddresses: {
-            56: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+            56: '0x55d398326f99059fF775485246999027B3197955',
         },
         acceleratorAddresses: {
             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
