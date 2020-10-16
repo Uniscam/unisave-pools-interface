1c3db17c72c3edaf37fca9e2ecc87f42224b57d2
Ayaka Neko <neko@ayaka.moe>
2020-10-16 14:35:11 +0800

    :fire: Removed VestNFT

--- src/components/TopBar/components/Nav.tsx
+++ src/components/TopBar/components/Nav.tsx
@@ -11,9 +11,6 @@ const Nav: React.FC = () => {
       <StyledLink exact activeClassName="active" to="/farms">
         Menu
       </StyledLink>
-      <StyledLink exact activeClassName="active" to="/vestnft">
-        VEST NFT
-      </StyledLink>
       <StyledLink exact activeClassName="active" to="/referral">
         Referral
       </StyledLink>
