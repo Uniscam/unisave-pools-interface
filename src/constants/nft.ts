9f9316c2a8cd5f8fc8bf97bcba23abd44024f278
Ayaka Neko <neko@ayaka.moe>
2020-10-16 16:11:50 +0800

    :ambulance: Fixed UI disappear issue and favicon issue

--- src/constants/nft.ts
+++ src/constants/nft.ts
@@ -1,7 +1,7 @@
 export const NFTs = [
   {
     addresses: {
-        97: '0xdf01427D5B686282BC5c0d4b7a75EC4D7F9078A7',
+        56: '0xdf01427D5B686282BC5c0d4b7a75EC4D7F9078A7',
     },
     name: 'Test NFT',
     symbol: 'TEST',
@@ -9,5 +9,5 @@ export const NFTs = [
 ]
 
 export const HoldingEnumerators = {
-    97: '0x97CA98a273CF3EEF77123D8AcD3A49188a1f826B',
+    56: '0x97CA98a273CF3EEF77123D8AcD3A49188a1f826B',
 }
