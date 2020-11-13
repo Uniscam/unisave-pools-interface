538161bdf52cbcee0dcfd7dfbd8dd6e4ac89e72e
lychees <lychees67@gmail.com>
2020-11-13 20:25:47 +0800

    Update pools.ts

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -6,6 +6,9 @@ export const Pools = [
         stakingTokenAddresses: {
             56: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
         },
+        acceleratorAddresses: {
+            56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+        },
         name: 'BUSD',
         symbol: 'BUSD',
         tokenSymbol: 'BUSD',
@@ -13,7 +16,8 @@ export const Pools = [
         isLpToken: false,
         icon: 'y3d',
         magnification: 0.5,
-    },
+    }
+    /*,
     {
         poolAddresses: {
             56: '0xB10fD0F525cBDeCef941a594dD7808427B3e87e2',
@@ -29,7 +33,6 @@ export const Pools = [
         icon: 'y3d',
         magnification: 0.5,
     },
-    /*
     {
         poolAddresses: {
             56: '0xA2A183416690C9199783fB67690d677Aa419E169',
