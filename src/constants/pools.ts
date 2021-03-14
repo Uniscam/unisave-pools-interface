85efed8493efe055b886866adf6a709d5d255382
xiaodao <lychees67@gmail.com>
2021-03-15 02:07:18 +0800

    Revert "Revert "Revert "Update pools.ts"""
    This reverts commit df2a1891a9291158ab3fba8db3bdea6b4270adad.

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -1,210 +1,210 @@
 export const Pools: any[] = [
-     {
-         poolAddresses: {
-             56: '0x07dB5Da96981A1937037c8F293Ae8efc43EE66b3',
-         },
-         stakingTokenAddresses: {
-             56: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },
-         name: 'BUSD',
-         symbol: 'BUSD',
-         tokenSymbol: 'BUSD',
-         isWBNB: false,
-         isLpToken: false,
-         icon: 'y3d',
-         magnification: 0.2,
-     },
-     {
-         poolAddresses: {
-             56: '0xB10fD0F525cBDeCef941a594dD7808427B3e87e2',
-         },
-         stakingTokenAddresses: {
-             56: '0x55d398326f99059fF775485246999027B3197955',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },        
-         name: 'USDT',
-         symbol: 'USDT',
-         tokenSymbol: 'USDT',
-         isWBNB: false,
-         isLpToken: false,
-         icon: 'y3d',
-         magnification: 0.2,
-     },
-     {
-         poolAddresses: {
-             56: '0xA2A183416690C9199783fB67690d677Aa419E169',
-         },
-         stakingTokenAddresses: {
-             56: '0x3e40fbee56d2298c8762770917315d748f74cf97',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },        
-         name: 'ETH/BUSD LP',
-         symbol: 'ETH/BUSD LP',
-         tokenSymbol: 'ETH/BUSD LP',
-         isWBNB: false,
-         isLpToken: true,
-         icon: 'y3d',
-         magnification: 1,
-     },
-     {
-         poolAddresses: {
-             56: '0x8f135bdb812e36e725ae1933f60bb2f96db48e88',
-         },
-         stakingTokenAddresses: {
-             56: '0xf4b69EE844a803b968378b1125C44DffF55BA0A8',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },        
-         name: 'ETH/USDT LP',
-         symbol: 'ETH/USDT  LP',
-         tokenSymbol: 'ETH/USDT LP',
-         isWBNB: false,
-         isLpToken: true,
-         icon: 'y3d',
-         magnification: 1,
-     },
-     {
-         poolAddresses: {
-             56: '0x4Ec9379Afd0c99f6Ed4786095a3dD8C491Be239C',
-         },
-         stakingTokenAddresses: {
-             56: '0xe9527c5ae16c0e2967ed8b967a7126a991b8903d',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },        
-         name: 'WBNB/BUSD LP',
-         symbol: 'WBNB/BUSD LP',
-         tokenSymbol: 'WBNB/BUSD LP',
-         isWBNB: false,
-         isLpToken: true,
-         icon: 'y3d',
-         magnification: 1,
-     },
-     {
-         poolAddresses: {
-             56: '0xD75998B880270276DB24a69DD751520064e270FC',
-         },
-         stakingTokenAddresses: {
-             56: '0x65522CD1534F3324E8Cd54D67F893fcC82293111',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },        
-         name: 'WBNB/USDT LP',
-         symbol: 'WBNB/USDT LP',
-         tokenSymbol: 'WBNB/USDT LP',
-         isWBNB: false,
-         isLpToken: true,
-         icon: 'y3d',
-         magnification: 1,
-     },
-     {
-         poolAddresses: {
-             56: '0xAe6AcA33844575F2DC410E6cd75587ab94d8b65A',
-         }, 
-         stakingTokenAddresses: {
-             56: '0xbef863E4bfd8372204a2064AEA4DEFbb8A803F86',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },
-         name: 'BNB/ETH LP',
-         symbol: 'BNB/ETH LP',
-         tokenSymbol: 'BNB/ETH LP',
-         isWBNB: false,
-         isLpToken: true,
-         icon: 'y3d',
-         nftSymbol: 'TEST',
-         magnification: 1,
-     },    
-     {
-         poolAddresses: {
-             56: '0x3f60462b65B37532F6A08846B7353813128Db907',
-         },
-         stakingTokenAddresses: {
-             56: '0x96a5335a3cf10e6429ee01b2701c6536587012e0',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },
-         name: 'USDT/BUSD LP',
-         symbol: 'USDT/BUSD LP',
-         tokenSymbol: 'USDT/BUSD LP',
-         isWBNB: false,
-         isLpToken: true,
-         icon: 'y3d',
-         nftSymbol: 'TEST',
-         magnification: 5,
-         p3d: 5,
-     },
-     {
-         poolAddresses: {
-             56: '0x3e8a3E3A7132311f66B36e6A91A6928C273f0077',
-         },
-         stakingTokenAddresses: {
-             56: '0x6e108E4bF4eaa30A1AdB933b98271Fe15d506045',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },
-         name: 'Y3D/BUSD LP',
-         symbol: 'Y3D/BUSD LP',
-         tokenSymbol: 'Y3D/BUSD LP',
-         isWBNB: false,
-         isLpToken: true,
-         icon: 'y3d',
-         nftSymbol: 'TEST',
-         magnification: 10,
-         p3d: 5
-     },
-     {
-         poolAddresses: {
-             56: '0x80d348bBcF1504e2D99850da482faE48138c8393',
-         },
-         stakingTokenAddresses: {
-             56: '0x68Ea08c0F9317EAda0192BE5A65a0Cb2968383e1',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },
-         name: 'WBNB/FOR LP',
-         symbol: 'WBNB/FOR LP',
-         tokenSymbol: 'WBNB/FOR LP',
-         isWBNB: false,
-         isLpToken: true,
-         icon: 'y3d',
-         nftSymbol: 'TEST',
-         magnification: 0.1,
-         p3d: 0
-     },
-     {
-         poolAddresses: {
-             56: '0x2656C7b0420cFdACCf884Ad4a21923628dC451da',
-         },
-         stakingTokenAddresses: {
-             56: '0x57EbF1d71a11519E4c5caB6DC164Bd9beA18Cf90',
-         },
-         acceleratorAddresses: {
-             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
-         },
-         name: 'WBNB/DEGO LP',
-         symbol: 'WBNB/DEGO LP',
-         tokenSymbol: 'WBNB/DEGO LP',
-         isWBNB: false,
-         isLpToken: true,
-         icon: 'y3d',
-         nftSymbol: 'TEST',
-         magnification: 0.1,
-         p3d: 0
-     },      
+//     {
+//         poolAddresses: {
+//             56: '0x07dB5Da96981A1937037c8F293Ae8efc43EE66b3',
+//         },
+//         stakingTokenAddresses: {
+//             56: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },
+//         name: 'BUSD',
+//         symbol: 'BUSD',
+//         tokenSymbol: 'BUSD',
+//         isWBNB: false,
+//         isLpToken: false,
+//         icon: 'y3d',
+//         magnification: 0.2,
+//     },
+//     {
+//         poolAddresses: {
+//             56: '0xB10fD0F525cBDeCef941a594dD7808427B3e87e2',
+//         },
+//         stakingTokenAddresses: {
+//             56: '0x55d398326f99059fF775485246999027B3197955',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },        
+//         name: 'USDT',
+//         symbol: 'USDT',
+//         tokenSymbol: 'USDT',
+//         isWBNB: false,
+//         isLpToken: false,
+//         icon: 'y3d',
+//         magnification: 0.2,
+//     },
+//     {
+//         poolAddresses: {
+//             56: '0xA2A183416690C9199783fB67690d677Aa419E169',
+//         },
+//         stakingTokenAddresses: {
+//             56: '0x3e40fbee56d2298c8762770917315d748f74cf97',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },        
+//         name: 'ETH/BUSD LP',
+//         symbol: 'ETH/BUSD LP',
+//         tokenSymbol: 'ETH/BUSD LP',
+//         isWBNB: false,
+//         isLpToken: true,
+//         icon: 'y3d',
+//         magnification: 1,
+//     },
+//     {
+//         poolAddresses: {
+//             56: '0x8f135bdb812e36e725ae1933f60bb2f96db48e88',
+//         },
+//         stakingTokenAddresses: {
+//             56: '0xf4b69EE844a803b968378b1125C44DffF55BA0A8',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },        
+//         name: 'ETH/USDT LP',
+//         symbol: 'ETH/USDT  LP',
+//         tokenSymbol: 'ETH/USDT LP',
+//         isWBNB: false,
+//         isLpToken: true,
+//         icon: 'y3d',
+//         magnification: 1,
+//     },
+//     {
+//         poolAddresses: {
+//             56: '0x4Ec9379Afd0c99f6Ed4786095a3dD8C491Be239C',
+//         },
+//         stakingTokenAddresses: {
+//             56: '0xe9527c5ae16c0e2967ed8b967a7126a991b8903d',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },        
+//         name: 'WBNB/BUSD LP',
+//         symbol: 'WBNB/BUSD LP',
+//         tokenSymbol: 'WBNB/BUSD LP',
+//         isWBNB: false,
+//         isLpToken: true,
+//         icon: 'y3d',
+//         magnification: 1,
+//     },
+//     {
+//         poolAddresses: {
+//             56: '0xD75998B880270276DB24a69DD751520064e270FC',
+//         },
+//         stakingTokenAddresses: {
+//             56: '0x65522CD1534F3324E8Cd54D67F893fcC82293111',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },        
+//         name: 'WBNB/USDT LP',
+//         symbol: 'WBNB/USDT LP',
+//         tokenSymbol: 'WBNB/USDT LP',
+//         isWBNB: false,
+//         isLpToken: true,
+//         icon: 'y3d',
+//         magnification: 1,
+//     },
+//     {
+//         poolAddresses: {
+//             56: '0xAe6AcA33844575F2DC410E6cd75587ab94d8b65A',
+//         }, 
+//         stakingTokenAddresses: {
+//             56: '0xbef863E4bfd8372204a2064AEA4DEFbb8A803F86',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },
+//         name: 'BNB/ETH LP',
+//         symbol: 'BNB/ETH LP',
+//         tokenSymbol: 'BNB/ETH LP',
+//         isWBNB: false,
+//         isLpToken: true,
+//         icon: 'y3d',
+//         nftSymbol: 'TEST',
+//         magnification: 1,
+//     },    
+//     {
+//         poolAddresses: {
+//             56: '0x3f60462b65B37532F6A08846B7353813128Db907',
+//         },
+//         stakingTokenAddresses: {
+//             56: '0x96a5335a3cf10e6429ee01b2701c6536587012e0',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },
+//         name: 'USDT/BUSD LP',
+//         symbol: 'USDT/BUSD LP',
+//         tokenSymbol: 'USDT/BUSD LP',
+//         isWBNB: false,
+//         isLpToken: true,
+//         icon: 'y3d',
+//         nftSymbol: 'TEST',
+//         magnification: 5,
+//         p3d: 5,
+//     },
+//     {
+//         poolAddresses: {
+//             56: '0x3e8a3E3A7132311f66B36e6A91A6928C273f0077',
+//         },
+//         stakingTokenAddresses: {
+//             56: '0x6e108E4bF4eaa30A1AdB933b98271Fe15d506045',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },
+//         name: 'Y3D/BUSD LP',
+//         symbol: 'Y3D/BUSD LP',
+//         tokenSymbol: 'Y3D/BUSD LP',
+//         isWBNB: false,
+//         isLpToken: true,
+//         icon: 'y3d',
+//         nftSymbol: 'TEST',
+//         magnification: 10,
+//         p3d: 5
+//     },
+//     {
+//         poolAddresses: {
+//             56: '0x80d348bBcF1504e2D99850da482faE48138c8393',
+//         },
+//         stakingTokenAddresses: {
+//             56: '0x68Ea08c0F9317EAda0192BE5A65a0Cb2968383e1',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },
+//         name: 'WBNB/FOR LP',
+//         symbol: 'WBNB/FOR LP',
+//         tokenSymbol: 'WBNB/FOR LP',
+//         isWBNB: false,
+//         isLpToken: true,
+//         icon: 'y3d',
+//         nftSymbol: 'TEST',
+//         magnification: 0.1,
+//         p3d: 0
+//     },
+//     {
+//         poolAddresses: {
+//             56: '0x2656C7b0420cFdACCf884Ad4a21923628dC451da',
+//         },
+//         stakingTokenAddresses: {
+//             56: '0x57EbF1d71a11519E4c5caB6DC164Bd9beA18Cf90',
+//         },
+//         acceleratorAddresses: {
+//             56: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7'
+//         },
+//         name: 'WBNB/DEGO LP',
+//         symbol: 'WBNB/DEGO LP',
+//         tokenSymbol: 'WBNB/DEGO LP',
+//         isWBNB: false,
+//         isLpToken: true,
+//         icon: 'y3d',
+//         nftSymbol: 'TEST',
+//         magnification: 0.1,
+//         p3d: 0
+//     },      
 
 ]
